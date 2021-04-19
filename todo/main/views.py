443dde8836de8d0035cf79fe803d0e318aa683ef
Taras Svynovei <81097696+svnoway@users.noreply.github.com>
2021-04-20 00:03:10 +0300

    edit at 20.04

--- todo/main/views.py
+++ todo/main/views.py
@@ -37,6 +37,7 @@ class RegisterPage(FormView):
 class Task(LoginRequiredMixin, ListView):
     model = Datask
     context_object_name = "taski"
+    ordering = ['-created']
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
